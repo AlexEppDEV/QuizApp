4b1bd6676cde2cb76874c311bc35a0b0ce548422
AlexE.Dev <locha8472@t-online.de>
2026-04-09 17:33:28 +0200

    feat : new function for buttons refactor: simplify auth logic chore: inport new pingers style: created new class html: created new tag and container script: created new file

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// variable
+let startQuestion = 0;
+
+
+function init() {
+    loadQuest();
+};
+
+function loadQuest() {
+    let answersQuest = " ";
+    
+    if (startQuestion >= questions.length) {
+        console.log('ende')
+        startQuestion = 0;
+        init(); 
+    }
+    else {
+        let answersQuestion = questions[startQuestion].question;
+        let questNumber = startQuestion + 1;
+        for (let index = 0; index < questions[startQuestion].answers.length; index++) {
+        answersQuest = questions[startQuestion].answers[index];
+        document.getElementById('answers'+'_'+ index).innerText = answersQuest;
+    };
+        document.getElementById('answersQuestionID').innerText = answersQuestion;
+        document.getElementById('quest-max-number').innerText = questions.length;
+        document.getElementById('quest-number').innerText = questNumber;
+    }
+    
+};
+
+function answers(selector) {
+    let rightAnswersQuestion = questions[startQuestion].correctAnswer;
+    let exportNumber = selector.slice(-1);
+
+    if ( exportNumber == questions[startQuestion].correctAnswer) {   
+        document.getElementById('answers_' + exportNumber).classList.add('bg-success');  
+    }
+    else {
+        document.getElementById('answers_' + exportNumber).classList.add('bg-danger');
+        document.getElementById('answers_' +rightAnswersQuestion).classList.add('bg-success');
+    }
+    document.getElementById('next-answers').disabled = false;
+};
+
+function nextQuestion() {
+    startQuestion++;
+    loadQuest();
+    removeButton();
+};
+
+function removeButton() { 
+    document.getElementById('next-answers').disabled = true;
+
+    for (let index = 0; index < questions[startQuestion].answers.length; index++) {
+        answersQuest = questions[startQuestion].answers[index];
+        document.getElementById('answers_' + index).classList.remove('bg-success');
+        document.getElementById('answers_' + index).classList.remove('bg-danger');
+    };
+
+};
---
+++ scripts/array.js
@@ -0,0 +1,52 @@
+let questions = [
+  {
+    question: "Wer ist der Captain der USS Enterprise in Star Trek: The Next Generation?",
+    answers: [
+        "James T. Kirk",
+        "Jean-Luc Picard",
+        "Benjamin Sisko",
+        "Kathryn Janeway"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: "Welcher Charakter ist ein Android?",
+    answers: [
+        "Data",
+        "Worf",
+        "Geordi La Forge",
+        "Riker"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Welche Spezies sind die Erzfeinde der Föderation und Teil eines Kollektivs?",
+    answers: [
+        "Klingonen",
+        "Romulaner",
+        "Borg",
+        "Cardassianer"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "Was trägt Geordi La Forge anstelle von normalen Augen?",
+    answers: [
+        "Ein Implantat",
+        "Einen Scanner",
+        "Eine Datenbrille",
+        "Ein VISOR"
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "Wie heißt das Raumschiff der Serie?",
+    answers: [
+        "USS Voyager",
+        "USS Defiant",
+        "USS Enterprise-D",
+        "USS Discovery"
+    ],
+    correctAnswer: 2
+  }
+];
