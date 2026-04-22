acad2bada97908a426f8ddec5f9860145db21d28
AlexE.Dev <locha8472@t-online.de>
2026-04-22 09:29:16 +0200

    feat: Add Bootstrap progress bar for question progress
    - Added HTML div for Bootstrap progress bar to track quiz progress
    - Implemented progressBar() function to update progress dynamically

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // variable
 let startCategory = 0; // nummer für die categorys
 let startQuestion = 0; // nummer für start index von der frage
-// let keysQuestion = 0;  // nummer für
+let rightAnswers = 0;
 let currentQuestions = []; // Speichert das aktuelle Fragen-Array 
 
 
@@ -16,6 +16,7 @@ function navCategory(selector) {
     startCategory = Number(selector.split('-')[1]); // "0" → 0
     startPage(startCategory);
 };
+
 
 // start seite für starten von quiz
 function startPage(startCategory) {
@@ -40,16 +41,16 @@ function startChallenge() {
     // inhalt aus objekt raus gelesen
     let keys = Object.keys(questions);
     let keysCategory = keys[exportNumber];
-    // let keysQuestion = questions[keysCategory];
+    rightAnswers = 0;
+    document.getElementById('quest_number_right').innerText = rightAnswers; // reset von richtigen fragen
     currentQuestions = questions[keysCategory]; // Speichere das Fragen-Array global
-
 
     document.getElementById('startScreen').style = 'display: none;';
     document.getElementById('questionBody').style = '';
       if (!startCategory) {
         nameCategory = startCategory;
     }
-    // loadQuest(keysQuestion);
+
     loadQuest(currentQuestions); // Übergib currentQuestions
 };
 
@@ -62,19 +63,23 @@ function loadQuest(keysQuestion) {
         document.getElementById('endScreen').style = '';
         document.getElementById('questionBody').style = 'display: none;';
         startQuestion = 0;
-   
     }
     else {
         let answersQuestion = keysQuestion[startQuestion].question;
         let questNumber = startQuestion + 1;
+
         for (let index = 0; index < keysQuestion[startQuestion].answers.length; index++) {
         answersQuest = keysQuestion[startQuestion].answers[index];
         document.getElementById('answers'+'_'+ index).innerText = answersQuest;
-    };
+        };
+        
         document.getElementById('answersQuestionID').innerText = answersQuestion;
         document.getElementById('quest-max-number').innerText = keysQuestion.length;
+        document.getElementById('quest_max_number_right').innerText = keysQuestion.length;
         document.getElementById('quest-number').innerText = questNumber;
-    }  
+        let progressValueMax = keysQuestion.length;
+        progressBar(progressValueMax, questNumber);
+    }
 };
 
 
@@ -82,22 +87,24 @@ function loadQuest(keysQuestion) {
 function answers(startCategory) {
     let exportNumber = Number(startCategory.split('-')[1]); // "answers-0" → 0
     let rightAnswersQuestion = currentQuestions[startQuestion].correctAnswer;
-    // let exportNumber = startCategory;
 
     if ( exportNumber == currentQuestions[startQuestion].correctAnswer) {   
-        document.getElementById('answers_' + exportNumber).classList.add('bg-success');  
+        document.getElementById('answers_' + exportNumber).classList.add('bg-success');
+        rightAnswers++
+        document.getElementById('quest_number_right').innerText = rightAnswers;
     }
     else {
         document.getElementById('answers_' + exportNumber).classList.add('bg-danger');
         document.getElementById('answers_' +rightAnswersQuestion).classList.add('bg-success');
     }
+
     document.getElementById('next-answers').disabled = false;
 };
 
 
 // weiter zu nächste frage gehen
 function nextQuestion() {
-    startQuestion++;
+    startQuestion++;   
     loadQuest(currentQuestions);
     removeButton();
 };
@@ -115,10 +122,9 @@ function removeButton() {
 
 
 // die Category wiederholen
-function replayCategory() {
+function replayCategory() { 
     selector = startCategory;
     startPage(selector);
-
 };
 
 
@@ -129,9 +135,17 @@ function shareCategory() {
 
     if ( startCategory >= key.length ) {
         startCategory = 0;
-        
     }
+
     document.getElementById('endScreen').style = 'display: none;';
     document.getElementById('questionBody').style = '';
     startPage(startCategory);
+};
+
+
+function progressBar(progressValueMax, questNumber) {
+    let progressValueCalc = 100 / progressValueMax;
+    progressValueCalc = progressValueCalc * questNumber;
+    document.querySelector(".progress-bar").innerText = Math.round(progressValueCalc) + "%";
+    document.querySelector(".progress-bar").style.width = `${progressValueCalc}%`;
 };
